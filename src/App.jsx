320e7ba6ef3cb32e60be1b2996add0415a7ed7c3
RaulHernandez19 <163434295+RaulHernandez19@users.noreply.github.com>
2026-08-27 21:11:39 -0700

    se agrego header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import BarraSuperior from './components/BarraSuperior'
 import FondoIlustraciones from './components/FondoIlustraciones'
 import SeccionIntro from './components/SeccionIntro'
 import Carrusel from './components/Carrusel'
@@ -6,24 +7,28 @@ import { carruseles } from './data/carruseles'
 
 export default function App() {
   return (
-    <div className="relative flex flex-1 flex-col overflow-hidden bg-white">
-      <FondoIlustraciones />
+    <>
+      <BarraSuperior />
 
-      <main className="relative mx-auto flex w-full max-w-[var(--ancho-contenido)] flex-1 flex-col gap-2 px-8 py-6">
-        <SeccionIntro />
+      <div className="relative flex flex-1 flex-col overflow-hidden bg-white">
+        <FondoIlustraciones />
 
-        <div className="flex flex-col gap-8">
-          {carruseles.map((carrusel, i) => (
-            <Carrusel
-              key={carrusel.id}
-              carrusel={carrusel}
-              lado={i % 2 ? 'derecha' : 'izquierda'}
-            />
-          ))}
-        </div>
-      </main>
+        <main className="relative mx-auto flex w-full max-w-[var(--ancho-contenido)] flex-1 flex-col gap-2 px-8 py-6">
+          <SeccionIntro />
 
-      <PieContacto />
-    </div>
+          <div className="flex flex-col gap-8">
+            {carruseles.map((carrusel, i) => (
+              <Carrusel
+                key={carrusel.id}
+                carrusel={carrusel}
+                lado={i % 2 ? 'derecha' : 'izquierda'}
+              />
+            ))}
+          </div>
+        </main>
+
+        <PieContacto />
+      </div>
+    </>
   )
 }
---
+++ src/components/BarraSuperior.tsx
@@ -0,0 +1,4 @@
+/** Barra superior negra, fija arriba de la pantalla al hacer scroll. */
+export default function BarraSuperior() {
+  return <header className="sticky top-0 z-50 h-14 bg-blue-900" />
+}
